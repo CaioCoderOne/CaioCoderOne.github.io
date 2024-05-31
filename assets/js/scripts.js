c22a488b7e232ba6db373c760280f8ec8642a9b0
CaioCoder <140705878+CaioCoderOne@users.noreply.github.com>
2024-05-31 04:19:03 -0300

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,17 +5,24 @@ const completedSlider = document.getElementById('completed-slider');
 const upcomingSlider = document.getElementById('upcoming-slider');
 
 function showSlide(slider, index) {
-    const slides = slider.getElementsByClassName('slide');
+    const slides = slider.getElementsByClassName('card');
     for (let i = 0; i < slides.length; i++) {
-        slides[i].style.transform = `translateX(-${index * 100}%)`;
+        slides[i].style.transform = `translateX(-${index * 220}px)`;
     }
 }
 
 function nextSlide() {
-    completedIndex = (completedIndex + 1) % completedSlider.getElementsByClassName('slide').length;
-    upcomingIndex = (upcomingIndex + 1) % upcomingSlider.getElementsByClassName('slide').length;
+    completedIndex = (completedIndex + 1) % (completedSlider.getElementsByClassName('card').length / 5);
+    upcomingIndex = (upcomingIndex + 1) % (upcomingSlider.getElementsByClassName('card').length / 5);
     showSlide(completedSlider, completedIndex);
     showSlide(upcomingSlider, upcomingIndex);
 }
 
 setInterval(nextSlide, 3000); // Slide changes every 3 seconds
+
+// Rotate card on click
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('click', () => {
+        card.classList.toggle('flipped');
+    });
+});
